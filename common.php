4c76d35216da27f990b432956292274ab18276b3
Benjamin HUBERT <benjamin@alpixel.fr>
2016-05-23 17:24:42 +0200

    added bower functions

--- common.php
+++ common.php
@@ -11,3 +11,31 @@ task('maintenance:enable', function () {
 task('maintenance:disable', function () {
     run("rm -f {{current}}/web/system/maintenance.html");
 })->desc('Désactivation de la page de maintenance');
+
+
+task('bower:update', function () {
+    $bowerPath = __DIR__ . '/../../../bower.json';
+
+    if (!is_file($bowerPath)) {
+        throw new InvalidArgumentException("Can't find bower.json");
+    }
+
+    $bowerContent = file_get_contents($bowerPath);
+    $bowerContent = json_decode($bowerContent);
+
+    $dependencies = [];
+    foreach ($bowerContent->dependencies as $key => $version) {
+        $dependencies[] = $key;
+    }
+
+    $package = null;
+    while (!in_array($package, $dependencies)) {
+        $package = ask('Quel paquet dois-je mettre à jour ?', null);
+    }
+
+    run("cd {{current}} && bower update ".$package);
+});
+
+task('bower:list', function () {
+    writeln(run("cd {{current}} && bower list"));
+});
